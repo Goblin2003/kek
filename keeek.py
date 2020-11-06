7906f663f08fefeed0c6d6a458cf971fcb73b421
Van Darkholme\Master Van <73282658+Rose-Crabel@users.noreply.github.com>
2020-11-06 10:53:58 +0300

    Update keeek.py
    ОООБНОВААА

--- keeek.py
+++ keeek.py
@@ -1,5 +1,8 @@
-x1=int(input(""))
-x2=int(input(""))
-x3=int(input(""))
-y=((x1*x1)+(x2*x2)+(x3*x3))-100
+x = int(input(' '))
+y = (x*x*x) - 64*x*((x*x*x) / (x*x))
+print(y)
+import math
+x1= int(input("Введите 1 число"))
+x2= int(input("Введите 2 число"))
+y= math.sqrt((x1*x2)*(x1*x2))
 print(y)
