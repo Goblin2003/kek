bf45dff6184c1e31878470a9a2a6299b4100fd0e
Alla41 <73988011+Alla41@users.noreply.github.com>
2020-11-06 10:44:55 +0300

    Update keeek.py

--- keeek.py
+++ keeek.py
@@ -1,6 +1,5 @@
-x = int(input(' '))
-y = (x*x*x) - 64*x*((x*x*x) / (x*x))
+x1=int(input(""))
+x2=int(input(""))
+x3=int(input(""))
+y=((x1*x1)+(x2*x2)+(x3*x3))-100
 print(y)
-import math
-x1= int(input("Введите 1 число"))
-x2= int(input("Введите 2 число"))
